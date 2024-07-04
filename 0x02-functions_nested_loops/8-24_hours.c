b5ae49b8226d85934d67f936d5bac14a04b273d4
geek <elmainiaymane03@gmail.com>
2024-07-04 16:17:19 +0100

    last_digit && 8-24_hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+void jack_bauer(void)
+{
+	int h = 0;
+	int m = 0;
+
+	for (h = 0; h < 24; h++) {
+		for (m = 0; m < 60; m++) {
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar(10);
+		}
+	}
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++) {
+		for (j = 0; j <= 9; j++) {
+		}
+	}
+}
