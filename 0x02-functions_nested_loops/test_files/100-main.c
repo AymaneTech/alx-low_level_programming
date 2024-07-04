c53c7dae93a928f50d7c41b0d2287c671cc89771
geek <elmainiaymane03@gmail.com>
2024-07-04 17:20:58 +0100

    100-times_table

---
+++ 0x02-functions_nested_loops/test_files/100-main.c
@@ -0,0 +1,13 @@
+#include "../main.h"
+
+int main()
+{
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+	return (0);
+}
