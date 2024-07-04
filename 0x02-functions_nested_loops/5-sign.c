59b047ebc930d24284ebfc3193d7889ca6b09caf
geek <elmainiaymane03@gmail.com>
2024-07-04 15:37:40 +0100

    finish the 5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,12 +4,12 @@ int print_sign(int c)
 {
 	if (c == 0) {
 		_putchar('0');
-		return 0;
+		return (0);
 	}
 	if (c < 0) {
 		_putchar('-');
-		return -1;
+		return (-1);
 	}
 	_putchar('+');
-	return 1;
+	return (1);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,5 +9,6 @@ void pirnt_alphabet_x10(void);
 int  _islower(int c);
 int  _isalpha(int c);
 int  print_sign(int n);
+int  _abs(int);
 
 #endif
